b46e47b0d3543bfdd85d8bf94b95571c37fa0314
aniebietafia <aniebietafia87@gmail.com>
2022-12-24 13:05:53 +0100

    added middleware for route and connection to mongodb

--- app.js
+++ app.js
@@ -2,9 +2,18 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const bodyParser = require("body-parser");
 
 // Importing custom files
 const MONGODB_CONNECTION = require("./db/connect");
+const userRoute = require("./routes/user");
+
+// middleware
+app.use(express.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use("/blogs-api/user", userRoute);
 
 const PORT = process.env.PORT || 3000;
 
