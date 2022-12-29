61394b54c92538e6e2f3c73a8473dbd314c8b097
aniebietafia <aniebietafia87@gmail.com>
2022-12-29 16:50:30 +0100

    Added new features[Rate Limiter]

--- app.js
+++ app.js
@@ -2,29 +2,58 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("express-async-errors");
 const bodyParser = require("body-parser");
+
+// Extra Security Packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xssClean = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 100, //limits each IP to 100 requests per 'window' (here, per 15 minutes)
+  standardHeaders: true, //Return rate limit info in the 'Rate Limit' headers
+  legacyHeaders: false, // Disable the X-RateLimit headers
+});
 
 // Importing custom files
 const MONGODB_CONNECTION = require("./db/connect");
-const userRoute = require("./routes/user");
+const authUser = require("./middleware/authentication");
+const authRoute = require("./routes/auth");
+const blogsRoute = require("./routes/blogs");
+
+// Error Handling
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/errorHandler");
 
 // middleware
-app.use(express.json);
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(xssClean());
+
+app.set("trust proxy", 1);
+app.use(limiter);
 
 // Routes
-app.use("/blogs-api/user", userRoute);
+app.use("/blogs-api/auth", authRoute);
+app.use("/blogs-api/blogs", authUser, blogsRoute);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Connecting to database and starting the server
-const start = async () => {
+const startServer = async () => {
   try {
     // MongoDB URI
-    MONGODB_CONNECTION(process.env.MONGODB_URI);
+    await MONGODB_CONNECTION(process.env.MONGODB_URI);
     app.listen(PORT, () => console.log(`Starting server port: ${PORT}`));
   } catch (error) {
     console.log(error);
   }
 };
-start();
+startServer();
