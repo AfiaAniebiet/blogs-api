997a8cb86429d747ddf1601571f3293627762c8f
aniebietafia <aniebietafia87@gmail.com>
2022-12-29 16:54:08 +0100

    Updated the Blogs and User schemas

---
+++ models/blogs.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const blogsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Provide Blog title"],
+      unique: [true, "Title exists already"],
+    },
+    blogImage: {
+      type: String,
+      required: [true, "Image Url is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Describe the blog!"],
+    },
+    blogBody: {
+      type: String,
+      required: [true, "Blog body is required"],
+    },
+    tags: {
+      type: String,
+      enum: [
+        "Technology",
+        "Sports",
+        "Religion",
+        "Politics",
+        "Economy",
+        "Nature",
+      ],
+      required: [true, "Select a category from the list"],
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Author's name is required"],
+    },
+    state: {
+      type: String,
+      enum: ["Draft", "Published"],
+      default: "Draft",
+    },
+    read_count: {
+      type: Number,
+    },
+    reading_time: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", blogsSchema);
--- models/user.js
+++ models/user.js
@@ -1,26 +1,35 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const userSchema = new mongoose.Schema({
   first_name: {
     type: String,
     required: [true, "Please provide your first name."],
+    minLength: 2,
+    maxLength: 30,
   },
   last_name: {
     type: String,
     required: [true, "Please provide your last name."],
+    minLength: 2,
+    maxLength: 30,
   },
   gender: {
     type: String,
+    enum: ["Male", "Female"],
   },
   email: {
     type: String,
     required: [true, "Please provide a valid email"],
+    unique: [true, "Email already exists"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
   },
   password: {
-    type: String,
-    required: true,
-  },
-  confirm_password: {
     type: String,
     required: true,
   },
@@ -29,5 +38,39 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
   },
 });
+
+// Mongoose middleware to hash password - version 1
+/**
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+**/
+
+// Mongoose middleware to hash password - version 2
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Generate token using instance method
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      first_name: this.first_name,
+      last_name: this.last_name,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_DURATION }
+  );
+};
+
+// Check entered password to stored password at login
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  const isMatch = bcrypt.compare(enteredPassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", userSchema);
